0c5b851e692d4e2073f0989b14369657e007e8d9
Fred Ojure <fredojure@hotmail.com>
2020-06-30 09:49:49 +0300

    Created run.py to run the application

--- run.py
+++ run.py
@@ -1,2 +1,31 @@
+#!c:\Python38\lib python3.8
+
 from user import User
 from credential import Credential
+
+def create_user():
+    '''
+    Function to create users
+    '''
+    new_user = User(name, password)
+    return new_user
+
+def create_credential():
+    '''
+    Function to create user's credential
+    '''
+    new_credential = Credential(name)
+    return new_credential
+
+def save_user():
+    '''
+    Funtion to save user
+    '''
+
+def main():
+    '''
+    Entry point to our application
+    '''
+    print("Hello: Welcome to our password Vault System")
+    print("-------------------------------------")
+    print("Select: 1 to Login or 2: To create an account")
